b1027cb1acc78055b42956e2fa8483097e271bf8
suryainno <suryabhargav@innoswift.com>
2020-02-08 13:35:45 +0530

    added HTTPS support for Node

--- Node/config.js
+++ Node/config.js
@@ -8,13 +8,15 @@ var enviornments = {};
 
 // staging object enviornment default
 enviornments.staging = {
-  'port' : 3000,
+  'httpPort' : 3000,
+  'httpsPort' : 3001,
   'envName' : 'staging'
 }
 
 // production object enviornment
 enviornments.production = {
-  'port' :5000,
+  'httpPort' :5000,
+  'httpsPort' : 5001,
   'envName' : 'production'
 }
 
--- Node/index.js
+++ Node/index.js
@@ -4,79 +4,101 @@
 
 //Dependencies
 var http = require('http');
+var https = require('https')
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
+var fs = require('fs');
 
-// server should respond to all requests
-var server = http.createServer(function(req,res){
+// Instansiating http server
+var httpServer = http.createServer(function(req,res){
+  unfiedServer(req,res);
+});
 
-//pasing url
-  var parseUrl = url.parse(req.url,true);
+//Start HTTP server
+httpServer.listen(config.httpPort,function(){
+  console.log('I am listening in http of port ' + config.httpPort);
+});
 
-//get the path
-  var path = parseUrl.pathname;
-  var trimedPath = path.replace(/^\/+|\/+$/g,'');
+// Instansiating https server
+var httpsServerOptions = {
+  'key' : fs.readFileSync('./https/key.pem'),
+  'cert' : fs.readFileSync('./https/cert.pem'),
+}
 
-//GEt the query string as an object
-  var queryStringObject = parseUrl.query;
+var httpsServer = https.createServer(httpsServerOptions,function(req,res){
+  unfiedServer(req,res);
+});
+
+//Start HTTPs server
+httpsServer.listen(config.httpsPort,function(){
+  console.log('I am listening in https of port ' + config.httpsPort);
+});
+
+//all the server logic for both http and https server
+var unfiedServer = function(req,res){
+
+  //pasing url
+    var parseUrl = url.parse(req.url,true);
+
+  //get the path
+    var path = parseUrl.pathname;
+    var trimedPath = path.replace(/^\/+|\/+$/g,'');
+
+  //GEt the query string as an object
+    var queryStringObject = parseUrl.query;
 
 
-//get the http meothod
-  var method = req.method.toLowerCase();
+  //get the http meothod
+    var method = req.method.toLowerCase();
 
-//Get the Header sent by user
-  var headers = req.headers;
+  //Get the Header sent by user
+    var headers = req.headers;
 
-//Get the payloads, if any
-var decoder = new StringDecoder('utf-8');
-var buffer = '';
-  req.on('data',function(data){
-    buffer += decoder.write(data);
-  });
-  req.on('end',function(){
-    buffer += decoder.end();
-
-    //chose the handler this req should goto if not found use not notFound
-    var choosenHandler = typeof(router[trimedPath]) !== 'undefined' ? router[trimedPath] : handlers.notFound ;
-
-    //conruct data object to send to handler
-    var data = {
-      'trimedPath' : trimedPath,
-      'queryStringObject' : queryStringObject,
-      'method' : method,
-      'headers' : headers,
-      'payload' : buffer
-    }
-
-    //route this requests specified in the router
-    choosenHandler(data,function(statusCode,payload){
-      //use the status code called back by handler, default to 200
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-      //use the pay load called back by the handler, or default to empty
-      payload = typeof(payload) == 'object' ? payload :{};
-
-      // Convert pay load to string
-      var payloadString = JSON.stringify(payload);
-
-      //return reaponse
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      //creating log
-        console.log('Returning this response',statusCode,payloadString);
+  //Get the payloads, if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+    req.on('data',function(data){
+      buffer += decoder.write(data);
     });
+    req.on('end',function(){
+      buffer += decoder.end();
+
+      //chose the handler this req should goto if not found use not notFound
+      var choosenHandler = typeof(router[trimedPath]) !== 'undefined' ? router[trimedPath] : handlers.notFound ;
+
+      //conruct data object to send to handler
+      var data = {
+        'trimedPath' : trimedPath,
+        'queryStringObject' : queryStringObject,
+        'method' : method,
+        'headers' : headers,
+        'payload' : buffer
+      }
+
+      //route this requests specified in the router
+      choosenHandler(data,function(statusCode,payload){
+        //use the status code called back by handler, default to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+        //use the pay load called back by the handler, or default to empty
+        payload = typeof(payload) == 'object' ? payload :{};
+
+        // Convert pay load to string
+        var payloadString = JSON.stringify(payload);
+
+        //return reaponse
+        res.setHeader('Content-Type', 'application/json');
+        res.writeHead(statusCode);
+        res.end(payloadString);
+
+        //creating log
+          console.log('Returning this response',statusCode,payloadString);
+      });
 
 
-  })
-});
-
-//Start server
-server.listen(config.port,function(){
-  console.log('I am listening in ' + config.port + " in "+ config.envName+" mode");
-});
+    })
+};
 
 //Define the handlers
 var handlers = {};
